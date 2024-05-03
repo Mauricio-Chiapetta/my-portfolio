c6bb2c84eab5a77f9fba05212b7cba8ac582b087
Matheus Barone <matheus.barone@raisin.com>
2024-05-03 10:21:24 +0200

    fix fontawesome imports

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -1,33 +1,41 @@
-const Skills = () => {
+import {
+    faHtml5,
+    faCss3Alt,
+    faJsSquare,
+    faReact,
+    faNodeJs,
+  } from "@fortawesome/free-brands-svg-icons";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  
+  const Skills = () => {
     return (
-        <div className="skills-container">
-            <h2>Skills</h2>
-            <div className="grid-skills">
-                <div className="skill-card html">
-                    <i className="fa-brands fa-html5 html-icon"></i>
-                    
-                    <p>HTML</p>
-                </div>
-                <div className="skill-card css">
-                    <i className="fa-brands fa-css3-alt css-icon"></i>
-                    <p>CSS</p>
-                </div>
-                <div className="skill-card js">
-                    <i className="fa-brands fa-js-square js-icon"></i>
-                    <p>JavaScript</p>
-                </div>
-                <div className="skill-card react">
-                    <i className="fa-brands fa-react react-icon"></i>
-                    <p>React</p>
-                </div>
-                <div className="skill-card node">
-                    <i className="fa-brands fa-node-js node-icon"></i>
-                    <p>Node</p>
-                </div>
-               
-            </div>
+      <div className="skills-container">
+        <h2>Skills</h2>
+        <div className="grid-skills">
+          <div className="skill-card html">
+            <FontAwesomeIcon icon={faHtml5} />
+            <p>HTML</p>
+          </div>
+          <div className="skill-card css">
+            <FontAwesomeIcon icon={faCss3Alt} className="css-icon" />
+            <p>CSS</p>
+          </div>
+          <div className="skill-card js">
+            <FontAwesomeIcon icon={faJsSquare} className="js-icon" />
+            <p>JavaScript</p>
+          </div>
+          <div className="skill-card react">
+            <FontAwesomeIcon icon={faReact} className="react-icon" />
+            <p>React</p>
+          </div>
+          <div className="skill-card node">
+            <FontAwesomeIcon icon={faNodeJs} className="node-icon" />
+            <p>Node</p>
+          </div>
         </div>
-    )
-}
-
-export default Skills;
+      </div>
+    );
+  };
+  
+  export default Skills;
+  
