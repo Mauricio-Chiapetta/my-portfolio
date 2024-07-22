592a2d3d05bd29ac8fb2a4c7628b3e19d2e0080b
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-07-22 17:33:18 -0300

    favicon

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,6 @@ import Projects from '@/app/components/Projects';
 import Contact from '@/app/components/Contact';
 import Footer from '@/app/components/footer';
 import Navbar from './components/navbar';
-
-
 const Home = () => {
 	return (
 		<>
@@ -17,7 +15,7 @@ const Home = () => {
 				<title>Mauricio's Portfolio</title>
 				<meta name="description" content="Mauricio's Portfolio" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="portfolio.ico" />
+				<link rel="icon" href="favicon.ico" />
 
 			</Head>
 
