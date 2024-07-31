04bf713ee736ff207b7b6995ec6645bb76e1080c
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-07-31 00:29:16 -0300

    ajustes css e lib nova de animação ("aos")

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,18 +1,26 @@
+"use client"
 import { projectData } from "./data.js";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
+import{useEffect} from "react"
 import { faLink } from "@fortawesome/free-solid-svg-icons"; // pacote novo de icones adicionado
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Projects = () => {
+
+  useEffect(() => {
+    Aos.init(); // Inicializando o AOS com duração de 2 segundos para as animações
+  }, []);
+
   return (
     <div id="Projects" className="projects-container">
       <h2>Projects:</h2>
       <div className="projects-grid">
         {projectData &&
           projectData.map((project) => (
-            <div className="project-card" key={project.id}>
+            <div data-aos="flip-left"  data-aos-duration="1000"className="project-card" key={project.id}>
               <div className="project-header">
                 <div className="small-icons">
                   <a href={project.gitHubLink} target="blank">
