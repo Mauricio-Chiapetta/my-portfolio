893cf2df9e0bf56a086cc3a68d097a4c5de75219
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-09-05 20:35:08 -0300

    add new project/css fix

--- src/app/components/data.js
+++ src/app/components/data.js
@@ -1,12 +1,22 @@
 // mudar o title,description e githublink para os meus projetos e colocar um link para poder abrir o projeto real
 import velha from '@/app/images/jogoDaVelha.png'
 import calculadora from '@/app/images/calculator.png'
+import todo from '@/app/images/todoApp.png'
 
 // arrumar imagens
 
 export const projectData = [
 	{
-		id: 1,
+		id:1,
+		title:'Todo App',
+		description:'Todo App utilizando React, Vite e Tailwindcss',
+		gitHubLink:'https://github.com/Mauricio-Chiapetta/TodoApp-w-tailwindcss',
+		liveDemo:'https://todo-app-w-tailwindcss.vercel.app/',
+		imagem:todo
+
+	},
+	{
+		id: 2,
 		title: 'Calculadora',
 		description:
 			'Calculadora simples feito em Html , Css , Javascript',
@@ -17,14 +27,15 @@ export const projectData = [
 
 	},
 	{
-		id: 2,
+		id: 3,
 		title: 'tic tac toe',
 		description:
 			'Jogo da velha com interface simples com o objetivo de estudo usando html,css e Javascript.',
 		gitHubLink: 'https://github.com/Mauricio-Chiapetta/tictactoe-js',
 		liveDemo: 'https://tictactoe-js-uvqf-e2dlxv8gl-mauriciochiapettas-projects.vercel.app/',
 		imagem: velha
-	},
+	}
+
 	// colocar mais projetos e imagens
 
 ];
