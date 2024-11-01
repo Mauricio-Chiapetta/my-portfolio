fbee296cabd9ee4c15d3728e98807c54281e0d1d
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-11-01 01:18:49 -0300

    add new project

--- src/app/components/data.js
+++ src/app/components/data.js
@@ -2,21 +2,31 @@
 import velha from '@/app/images/jogoDaVelha.png'
 import calculadora from '@/app/images/calculator.png'
 import todo from '@/app/images/todoApp.png'
+import landingPage from '@/app/images/TechConference.png'
 
 // arrumar imagens
 
 export const projectData = [
 	{
-		id:1,
-		title:'Todo App',
-		description:'Todo App utilizando React, Vite e Tailwindcss',
-		gitHubLink:'https://github.com/Mauricio-Chiapetta/TodoApp-w-tailwindcss',
-		liveDemo:'https://todo-app-w-tailwindcss.vercel.app/',
-		imagem:todo
+		id: 1,
+		title: 'Todo App',
+		description: 'Todo App utilizando React, Vite e Tailwindcss',
+		gitHubLink: 'https://github.com/Mauricio-Chiapetta/TodoApp-w-tailwindcss',
+		liveDemo: 'https://todo-app-w-tailwindcss.vercel.app/',
+		imagem: todo
 
 	},
 	{
 		id: 2,
+		title: 'Landing Page',
+		description:
+			'Landing page criada para uma conferência Tech! O site possui um modo claro e um modo escuro, e é aplicado de acordo com a preferência do sistema do usuário , foi utilizado nesse projeto Reactjs e TailwindCss',
+		gitHubLink: 'https://github.com/Mauricio-Chiapetta/ex6-websiteResponsivo',
+		liveDemo: 'https://techconference-landingpage.vercel.app/',
+		imagem: landingPage
+	},
+	{
+		id: 3,
 		title: 'Calculadora',
 		description:
 			'Calculadora simples feito em Html , Css , Javascript',
@@ -27,7 +37,7 @@ export const projectData = [
 
 	},
 	{
-		id: 3,
+		id: 4,
 		title: 'tic tac toe',
 		description:
 			'Jogo da velha com interface simples com o objetivo de estudo usando html,css e Javascript.',
@@ -36,6 +46,6 @@ export const projectData = [
 		imagem: velha
 	}
 
-	// colocar mais projetos e imagens
+
 
 ];
